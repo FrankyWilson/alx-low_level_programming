43206976f939e01b20736cf6c0f3d974bc61bb26
root <frankwilliamsugwu0@gmail.com>
2022-12-09 17:57:54 +0000

    hot fix size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-/**
- * main prints sizes of the matchin type
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 	char q;
@@ -12,10 +7,11 @@ int main(void)
 	long long int r;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(q));
-	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(w));
-	printf("Size of a long int: %lubyte(s)\n", (unsigned long)sizeof(e));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(r);)
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(q));                                                                        
+        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(w));                                                                        
+        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(e));                                                                    
+        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(r));                                                               
+        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));                                                                       
+        return (0);
+
 }
