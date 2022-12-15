b980ae6184cb5e04f1d412451b35d11e988374e1
oneMillioneCode <frankwilliamsugwu0@gmail.com>
2022-12-15 04:51:20 -0800

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,12 +15,12 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(i + '0');
 			}
-			else if (i >= 10)
+			else if (i > 10)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
 			}
-			else if (j <= 10)
+			else if (j < 10)
 			{
 				_putchar(':');
 				_putchar((j / 10) + '0');
