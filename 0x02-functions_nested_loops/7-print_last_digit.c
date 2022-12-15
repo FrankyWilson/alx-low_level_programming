49717f51e848c1641c1cb0dde00f5aa54f6553a0
oneMillioneCode <frankwilliamsugwu0@gmail.com>
2022-12-14 17:46:34 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit -> prints last digits
+ *
+ * @n: parameter
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n);
+{
+	if (n < 0)
+	{
+		n = -n;
+		return (n % 10);
+	}
+}
