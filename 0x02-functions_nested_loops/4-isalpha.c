a72484a74a299acc72e323dbed44dace121ea386
oneMillioneCode <frankwilliamsugwu0@gmail.com>
2022-12-14 16:35:37 -0800

    hot fix number 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  *
  * Return: 1 if c is a letter, lowercase of uppercase, returns 0 if otherwise
  */
-int _islower(int c)
+int _isalpha(int c);
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
