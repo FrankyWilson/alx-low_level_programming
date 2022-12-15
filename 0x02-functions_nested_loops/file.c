7a8bbbd2d0b2ce116f1c9f283a08c92e61c0002b
oneMillioneCode <frankwilliamsugwu0@gmail.com>
2022-12-15 08:07:21 -0800

    added number 11

---
+++ 0x02-functions_nested_loops/file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 -> check main.h file
+ * @n: An input iteger
+ *
+ * Return: Always serious
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n --)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
