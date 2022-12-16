94c5dfd8c81e0d0e53d8ee1a73a2aae4e5c33d07
oneMillioneCode <frankwilliamsugwu0@gmail.com>
2022-12-15 20:58:34 -0800

    added 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * print_times_table -> prints times tale for numbers
+ * @n: an input intger vale
+ * Return: Notheing
+ */
+void print_times_table(ing n)
+{
+	int i, j;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = i; j <= n; j++)
+				putformart(i * j);
+			_putchar('\n');
+		}
+	}
+}
+
+/**
+ * putformat -> formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+void putformat(int n)
+{
+	if (n <=9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putcahr(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putcahr(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
