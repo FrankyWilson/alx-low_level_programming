9c7740179c2bb540120914a1ca77a566275a1de1
oneMillioneCode <frankwilliamsugwu0@gmail.com>
2022-12-16 01:54:31 -0800

    added digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ */
+int _isdigit(int c)
+{
+	char x = '0';
+	int digit = 0;
+
+	for (; x <= '9'; x++)
+	{
+		if (i == c)
+		{
+			digit = 1;
+			break;
+		}
+	}
+
+	return (digit);
+}
