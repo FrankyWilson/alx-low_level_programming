89f22b67a4b30ee23fdfe4ede325fc8b99e7dd43
oneMillioneCode <frankwilliamsugwu0@gmail.com>
2022-12-15 21:20:21 -0800

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,12 +11,14 @@ void print_times_table(int n)
 	if (n > 0 && n < 15)
 	{
 		for (i = 0; i <= n; i++)
+		{
 
 			_putchar('0');
 
 			for (j = i; j <= n; j++)
 				putformat(i * j);
 			_putchar('\n');
+		}
 	}
 }
 
