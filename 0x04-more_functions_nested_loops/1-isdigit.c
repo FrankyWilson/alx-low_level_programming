e22e65c4018aa41e385be0affa073acb99aa6350
oneMillioneCode <frankwilliamsugwu0@gmail.com>
2022-12-16 02:04:56 -0800

    is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,20 +1,24 @@
 #include "main.h"
 /**
+ * _isdigit -> A function that checks for a digit (0 through 9).
  *
+ * @c: An input integer
+ *
+ * Return: 1 if c is a gigit or 0 otherwise
  */
 int _isdigit(int c)
 {
 	char x = '0';
-	int digit = 0;
+	int _isdigit = 0;
 
 	for (; x <= '9'; x++)
 	{
 		if (i == c)
 		{
-			digit = 1;
+			_isdigit = 1;
 			break;
 		}
 	}
 
-	return (digit);
+	return (_isdigit);
 }
