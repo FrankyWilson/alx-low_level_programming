c9575fd5e4da4c475c77179d56de45ec252442a3
frankywilson <frankwilliamsugwu0@gmail.com>
2022-12-22 01:07:34 +0000

    added number 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,16 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < n - 1)
+
+		if (i != (n - 1))
+		{
 			printf(", ");
+		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
