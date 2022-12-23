28d4bb88ac2887bb259726d032494a720423a362
frankywilson <frankwilliamsugwu0@gmail.com>
2022-12-23 10:33:12 +0000

    hot fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * reverse_array -> reversing an array
- * @: array a
- * n: an element of an array
+ * @a: array a
+ * @n: an element of an array
  */
 void reverse_array(int *a, int n)
 {
