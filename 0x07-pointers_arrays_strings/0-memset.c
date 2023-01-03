85067b6c971a72a997e5c4a116d557e5bcf275c7
root <FrankyWilson>
2023-01-03 08:36:57 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset -> memory
+ * @s: string 
+ * @b: char
+ * @n: an integer
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+#pointers and arrays
+
+pointers and how to use them
+multidimesional arrays and how to use them
+
