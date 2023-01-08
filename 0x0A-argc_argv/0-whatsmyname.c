721c212f300ca7e18da916db6f9b4046d41d274c
root <FrankyWilson>
2023-01-08 15:11:40 +0000

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: an array of a listed command
  * Return: 0 for success
  */
-int main(int arg _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
