21e07fd2f47e821022102691288003f004e12770
root <FrankyWilson>
2023-01-10 17:21:19 +0000

    hot fix

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 int _putchar;
-char *create_array(unisigned int size, char c);
+char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 
 #endif
